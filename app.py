ab1009d39d4e07dd17a9fb55100e6fb130b3e601
Hyun Woong <mr.woong0926@gmail.com>
2022-02-01 19:25:41 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=M&rtm=N&ymd=20210701',headers=headers)
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+genies = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+
+for genie in genies:
+        title = genie.select_one('td.info > a.title.ellipsis').text.strip()
+        a = title.replace('19금',' ')
+        rank = genie.select_one('td.number').text[0:2].strip()
+        singer = genie.select_one('td.info > a.artist.ellipsis').text.strip()
+        print(rank, a.strip(), singer)
